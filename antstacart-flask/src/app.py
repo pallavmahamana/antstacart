8827aaa3723f2cbdeb81b5c3d3a37d39279a6169
Pallav M <pallavmahamana@gmail.com>
2022-01-23 18:34:10 +0530

    first poc

--- antstacart-flask/src/app.py
+++ antstacart-flask/src/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, request
+from flask_restful import Resource, Api
+from datetime import datetime
+from util import InvoiceGenerator
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,9 +9,19 @@ api = Api(app)
 
 class GenerateInvoice(Resource):
     def post(self):
-        json_data = request.get_json()
-        print(json_data)
-        return "OK"
+        json_data = request.get_json(force=True)
+        order = InvoiceGenerator(json_data)
+        order.calculate_order()
+        return {"commodities":order._commodities,
+                "total_order":{
+                    "datetime": str(datetime.now()),
+                    "total_amount_without_discount": order._total_amount,
+                    "total_tax_amount": order._total_tax,
+                    "discount_applied": order.apply_discount(),
+                    "total_amount_with_discount": order._total_amount_with_discount
+
+                }}
+
 
 
 
@@ -18,4 +30,4 @@ api.add_resource(GenerateInvoice,'/invoice')
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.9',port=4455,debug=True)
+    app.run(host='0.0.0.0',port=4455,debug=True)
--- antstacart-flask/src/util.py
+++ antstacart-flask/src/util.py
@@ -5,7 +5,7 @@ tax_dict = {"Medicine":5,
             "Clothes":5,
             "Music":3,
             "Imported":18,
-            "Books":0}
+            "Book":0}
 
 
 #get tax on item category
@@ -22,8 +22,56 @@ def get_total_tax_onitems(quantity,itemCategory,itemprice) -> int:
 
 #get total price of items deducting tax
 def get_total_price_ofitems(quantity,itemCategory,itemprice) -> int:
-    return quantity*itemprice - get_total_tax_onitems(quantity,itemCategory,itemprice)
+    return quantity*itemprice + get_total_tax_onitems(quantity,itemCategory,itemprice)
 
+
+
+
+class InvoiceGenerator():
+    def __init__(self,json):
+        self._json = json
+        self._commodities = []
+        self._total_tax = 0.0
+        self._total_amount = 0.0
+        self._total_amount_with_discount = 0.0
+
+
+    def apply_discount(self):
+        if self._total_amount > 2000:
+            self._total_amount_with_discount = self._total_amount - 0.05 * self._total_amount
+            return True
+        else:
+            self._total_amount_with_discount = self._total_amount
+            return False
+
+
+    def calculate_order(self):
+        for item in self._json:
+            self._total_amount = self._total_amount + get_total_price_ofitems(item["quantity"],
+                                                                            item["itemCategory"],
+                                                                            item["price"])
+            self._total_tax = self._total_tax + get_total_tax_onitems(item["quantity"],
+                                                                            item["itemCategory"],
+                                                                            item["price"])
+
+
+
+            self._commodities.append(
+                {
+                    "item":item["item"],
+                    "itemCategory":item["itemCategory"],
+                    "quantity":item["quantity"],
+                    "itemprice":item["price"],
+                    "finalprice":get_total_price_ofitems(item["quantity"],
+                                                        item["itemCategory"],
+                                                        item["price"]),
+                    "tax_amount":get_total_tax_onitems(item["quantity"],
+                                                        item["itemCategory"],
+                                                        item["price"])
+                }
+            )
+        
+        
 
 
 
